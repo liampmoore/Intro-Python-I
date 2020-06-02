400d15a0c64bcadfd11e1212ed7402b65a331396
Liam Moore <liam.moore.dev@gmail.com>
2020-06-02 21:18:00 +0000

    finishes modules up to 14

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,10 +10,19 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
-
-# Open up a file called "bar.txt" (which doesn't exist yet) for
+with open('foo.txt') as f:
+         read_data = f.read()
+         print(read_data)
+print(f.closed)
+# Open up a file called "bar.txt" (which doesn't exist yet) fort
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('bar.txt', 'w') as f:
+         f.write("Hello\nworld\nhello")
+with open('bar.txt') as f:
+        read_data = f.read()
+        print(read_data)
+print(f.closed)
